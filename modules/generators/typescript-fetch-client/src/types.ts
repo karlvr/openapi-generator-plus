05b50387cc02349bbdd374ac148f15fe751bf18f
Karl von Randow <karl@xk72.com>
2020-02-06 10:57:48 +1300

    Initial import of typescript-fetch-client generator with templates from swagger-codegen

---
+++ modules/generators/typescript-fetch-client/src/types.ts
@@ -0,0 +1,17 @@
+import { CodegenOptions, CodegenRootContext } from 'openapi-generator-node-core'
+
+/**
+ * Options specific to Java that the user can provide to the code generation process.
+ */
+export interface CodegenOptionsTypescript extends CodegenOptions {
+	apiPackage: string
+	apiServiceImplPackage: string
+	modelPackage: string
+	invokerPackage: string
+	useBeanValidation?: boolean
+}
+
+export interface CodegenRootContextTypescript extends CodegenRootContext {
+	package: string
+	imports?: string[]
+}

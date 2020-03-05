2a7acae364ada23b3351ee35d399daa285520614
Karl von Randow <karl@xk72.com>
2020-03-06 08:20:30 +1300

    cli: improve error handling

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,4 +1,5 @@
 import SwaggerParser from 'swagger-parser'
+import { OpenAPI } from 'openapi-types'
 import { promises as fs } from 'fs'
 import { CodegenConfig, CodegenState, CodegenInitialOptions, processDocument } from '@openapi-generator-plus/core'
 import getopts from 'getopts'
@@ -25,50 +26,56 @@ function usage() {
 }
 
 export async function run() {
-	try {
-		const commandLineOptions = getopts(process.argv.slice(2), {
-			alias: {
-				config: 'c',
-				output: 'o',
-				generator: 'g',
-			},
-			unknown: (option) => {
-				console.log(`Unknown option: ${option}`)
-				return false
-			},
-		})
+	const commandLineOptions = getopts(process.argv.slice(2), {
+		alias: {
+			config: 'c',
+			output: 'o',
+			generator: 'g',
+		},
+		unknown: (option) => {
+			console.log(`Unknown option: ${option}`)
+			return false
+		},
+	})
 
-		if (commandLineOptions._.length === 0 || !commandLineOptions.output || !commandLineOptions.generator) {
-			usage()
-			process.exit(1)
-		}
-
-		const initialOptions = commandLineOptions.config ? await loadCommandLineConfig(commandLineOptions.config) : {}
-		initialOptions.output = commandLineOptions.output
-
-		const parser = new SwaggerParser()
-		const root = await parser.parse(commandLineOptions._[0])
-		const config: CodegenConfig = require(path.resolve(commandLineOptions.generator)).default
-
-		const state: CodegenState = {
-			parser,
-			root,
-			config,
-			options: config.options(initialOptions),
-			anonymousModels: {},
-		}
-
-		// console.log('refs', parser.$refs)
-		// return
-		
-		// console.log(JSON.stringify(api, null, 2))
-
-		const doc = processDocument(root, state)
-
-		await config.exportTemplates(doc, state)
-	} catch (error) {
-		console.error('API validation failed', error)
+	if (commandLineOptions._.length === 0 || !commandLineOptions.output || !commandLineOptions.generator) {
+		usage()
+		process.exit(1)
 	}
+
+	const initialOptions = commandLineOptions.config ? await loadCommandLineConfig(commandLineOptions.config) : {}
+	initialOptions.output = commandLineOptions.output
+
+	const parser = new SwaggerParser()
+	const apiPath = commandLineOptions._[0]
+
+	let root: OpenAPI.Document
+	try {
+		root = await parser.parse(apiPath)
+	} catch (error) {
+		console.error(`Failed to load API specification: ${apiPath} (${error.message})`)
+		process.exit(1)
+	}
+
+	let config: CodegenConfig
+	try {
+		config = require(path.resolve(commandLineOptions.generator)).default
+	} catch (error) {
+		console.error(`Failed to load generator module: ${commandLineOptions.generator} (${error.message})`)
+		process.exit(1)
+	}
+
+	const state: CodegenState = {
+		parser,
+		root,
+		config,
+		options: config.options(initialOptions),
+		anonymousModels: {},
+	}
+
+	const doc = processDocument(root, state)
+
+	await config.exportTemplates(doc, state)
 }
 
 run()

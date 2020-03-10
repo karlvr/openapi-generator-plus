58fb087452fff3ac53d5f037404886689edf8199
Karl von Randow <karl@xk72.com>
2020-03-10 21:07:38 +1300

    testing: fix jest config to ignore generated javascript files

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	/* testRegex so we don't run tests in the dist directory */
-	testRegex: '(/__tests__/.*|/src/.*(\\.|/)(test|spec))\\.[jt]sx?$',
+	testPathIgnorePatterns: [
+		'/node_modules/',
+		'/dist/',
+	],
 }

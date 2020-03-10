60871c7e15657671ee718f6f88b412b5f6b84642
Karl von Randow <karl@xk72.com>
2020-03-10 22:44:52 +1300

    core: OpenAPIv3 tests

---
+++ packages/core/__tests__/openapiv3.spec.ts
@@ -0,0 +1,74 @@
+import { createTestState } from './common'
+import { processDocument } from '../src/process'
+
+test('process document', async() => {
+	const state = await createTestState('openapiv3-1.yml')
+	processDocument(state)
+})
+
+test('parse info', async() => {
+	const state = await createTestState('openapiv3-1.yml')
+	const result = processDocument(state)
+
+	expect(result.info.description).toEqual('Lorem ipsum')
+	expect(result.info.version).toEqual('1.0.1')
+	expect(result.info.title).toEqual('Example')
+	expect(result.servers).toBeDefined()
+	expect(result.servers![0].url).toEqual('http://example.com/api/v1')
+	expect(result.servers![1].url).toEqual('https://example.com/api/v1')
+})
+
+test('parse operation params', async() => {
+	const state = await createTestState('openapiv3-1.yml')
+	const result = processDocument(state)
+
+	const group1 = result.groups[0]
+	const op1 = group1.operations[0]
+	expect(op1.name).toEqual('getTest1')
+	expect(op1.allParams).toBeDefined()
+	expect(op1.allParams!.length).toEqual(2)
+
+	const group2 = result.groups[1]
+	const op2 = group2.operations[0]
+	expect(op2.allParams!.length).toEqual(1)
+})
+
+test('parse operation response', async() => {
+	const state = await createTestState('openapiv3-1.yml')
+	const result = processDocument(state)
+
+	const group1 = result.groups[0]
+	const op1 = group1.operations[0]
+	expect(op1.name).toEqual('getTest1')
+	expect(op1.returnType).toEqual('object')
+	expect(op1.returnNativeType?.toString()).toEqual('Test1Response')
+
+	const group2 = result.groups[1]
+	const op2 = group2.operations[0]
+	expect(op2.returnType).toEqual('object')
+	expect(op2.returnNativeType?.toString()).toEqual('Test2Response')
+})
+
+test('parse groups', async() => {
+	const state = await createTestState('openapiv3-1.yml')
+	const result = processDocument(state)
+
+	expect(result.groups.length).toEqual(2)
+	const group1 = result.groups[0]
+	expect(group1.name).toEqual('test1')
+	expect(group1.path).toEqual('/test1')
+	expect(group1.operations.length).toEqual(2)
+
+	const op1 = group1.operations[0]
+	expect(op1.name).toEqual('getTest1')
+
+	const group2 = result.groups[1]
+	expect(group2.name).toEqual('test2')
+	expect(group2.operations.length).toEqual(1)
+
+	const op2 = group2.operations[0]
+	expect(op2.name).toEqual(state.generator.toOperationName('/test2', 'POST', state)) /* Uses default name */
+	// expect(op2.allParams!.length).toEqual(1)
+	// expect(op2.returnType).not.toBeDefined()
+	// expect(op2.returnNativeType).not.toBeDefined()
+})

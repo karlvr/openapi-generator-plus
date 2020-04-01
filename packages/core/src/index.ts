a1ce2ea750fa59dac4a65ad28c2d800ce485ae86
Karl von Randow <karl@xk72.com>
2020-04-01 21:54:06 +1300

    Revert "core: add baseGenerator"
    This reverts commit 70fbd576ef5753aef0fa72f9ced2932f77b37391.
    
    # Conflicts:
    #	packages/core/src/generators.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,7 +2,6 @@ export { InvalidModelError, processDocument } from './process'
 export * from './types'
 export * from './case-transforms'
 export * from './utils'
-export * from './generators'
 
 import * as allGroupingStrategies from './operation-grouping'
 export const GroupingStrategies = allGroupingStrategies

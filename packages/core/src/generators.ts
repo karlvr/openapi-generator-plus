a6e0e6a37a4c9ad9023a9876c72a0afbc90f7612
Karl von Randow <karl@xk72.com>
2020-04-02 08:04:57 +1300

    Revert "Revert "core: add baseGenerator""
    This reverts commit a1ce2ea750fa59dac4a65ad28c2d800ce485ae86.

---
+++ packages/core/src/generators.ts
@@ -0,0 +1,27 @@
+import { camelCase } from './case-transforms'
+import { CodegenOptions, CodegenGenerator } from './types'
+import pluralize from 'pluralize'
+
+/** Create a default operation name for operations that lack an operationId */
+function defaultOperationName(path: string, method: string): string {
+	/* Remove path variables from the path */
+	const sanitisedPath = path.replace(/\{[^}]*\}/g, '')
+	const combined = `${method.toLocaleLowerCase()}_${sanitisedPath}`
+	const sanitizedCombined = combined.replace(/[^a-zA-Z0-9]/g, '_').replace(/_+$/, '')
+	return camelCase(sanitizedCombined)
+}
+
+/**
+ * A partial generator implementation that should be the base of all generators.
+ * This enables the core to introduce new CodegenGenerator methods and add default
+ * implementations here, if appropriate.
+ */
+export function baseGenerator<O extends CodegenOptions>(): Pick<CodegenGenerator<O>, 'toIteratedModelName' | 'toModelNameFromPropertyName' | 'toOperationName'> {
+	return {
+		toIteratedModelName: (name, _, iteration) => `${name}${iteration + 1}`,
+		toModelNameFromPropertyName: (name, state) => {
+			return state.generator.toClassName(pluralize.singular(name), state)
+		},
+		toOperationName: defaultOperationName,
+	}
+}
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,7 @@ export { InvalidModelError, processDocument } from './process'
 export * from './types'
 export * from './case-transforms'
 export * from './utils'
+export * from './generators'
 
 import * as allGroupingStrategies from './operation-grouping'
 export const GroupingStrategies = allGroupingStrategies

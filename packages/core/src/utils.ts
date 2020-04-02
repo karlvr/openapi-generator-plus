463b34fcfa4e3448ca563daabf765c89ba5ea0bd
Karl von Randow <karl@xk72.com>
2020-04-02 13:49:10 +1300

    core: move compareHttpMethods to generator-common

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -33,30 +33,6 @@ export function stringLiteralValueOptions<O extends CodegenOptions>(state: Codeg
 	}
 }
 
-export function compareHttpMethods(a: string, b: string): number {
-	const aa = httpMethodToNumber(a)
-	const bb = httpMethodToNumber(b)
-	if (aa < bb) {
-		return -1
-	} else if (aa > bb) {
-		return 1
-	} else {
-		return 0
-	}
-}
-
-function httpMethodToNumber(method: string): number {
-	let i = 0
-	for (const aMethod in HttpMethods) {
-		if (method === aMethod) {
-			return i
-		}
-		i += 1
-	}
-
-	return i
-}
-
 export type CompareFunction<T> = (a: T, b: T) => number
 
 /**

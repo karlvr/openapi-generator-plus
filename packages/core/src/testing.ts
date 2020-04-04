43f4e55e4d802a67103d8fb936f4d99dfeaff984
Karl von Randow <karl@xk72.com>
2020-04-04 22:03:30 +1300

    core: add a function to help tests

---
+++ packages/core/src/testing.ts
@@ -0,0 +1,18 @@
+import { CodegenDocument, CodegenState, CodegenConfig, CodegenGeneratorConstructor } from '@openapi-generator-plus/types'
+import { constructGenerator, createCodegenState, createCodegenInput, createCodegenDocument } from '.'
+
+export interface CodegenResult<O> {
+	doc: CodegenDocument
+	state: CodegenState<O>
+}
+
+export async function createCodegenResult<O>(inputPath: string, config: CodegenConfig, generatorConstructor: CodegenGeneratorConstructor<O>): Promise<CodegenResult<O>> {
+	const generator = constructGenerator(generatorConstructor)
+	const state = createCodegenState(config, generator)
+	const input = await createCodegenInput(inputPath)
+	const doc = createCodegenDocument(input, state)
+	return {
+		doc,
+		state,
+	}
+}

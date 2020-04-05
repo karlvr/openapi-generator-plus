a7c259855de84cc8dfda00e49f3ba159f820e8e7
Karl von Randow <karl@xk72.com>
2020-04-06 09:06:04 +1200

    lint: improve test linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'extends': ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	'extends': ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:jest/recommended'],
 	'env': {
 		'node': true,
 		'jest': true,

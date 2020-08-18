151116798fa494a5972756e645ebb3eb202bbd41
Karl von Randow <karl@xk72.com>
2020-08-18 16:44:36 +1200

    core: documentation on indexed-type

--- packages/core/src/indexed-type.ts
+++ packages/core/src/indexed-type.ts
@@ -1,6 +1,6 @@
 /*
 Enable switching between POJOs and Maps for our indexed object type.
-You must also make the switch in @openapi-generator-plus/types types.ts
+You must also switch the definition of IndexedCollectionType in @openapi-generator-plus/types types.ts
 */
 export * from './objects'
 // export * from './maps'

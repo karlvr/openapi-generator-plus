a9f9f3f65fc72a243b0064eea6186687c9ea7b15
Karl von Randow <karl@xk72.com>
2020-08-18 15:35:23 +1200

    testing: fix property conflict test due to change in conflict resolution
    See dc03b4b0b72fd83f5190358434ed753425e87698

--- packages/core/src/__tests__/all-of-property-conflict.spec.ts
+++ packages/core/src/__tests__/all-of-property-conflict.spec.ts
@@ -1,6 +1,15 @@
 import { createTestDocument } from './common'
+import { CodegenPropertyType } from '@openapi-generator-plus/types'
+import * as idx from '../indexed-type'
 
-test('property conflict detected', async() => {
-	await expect(createTestDocument('all-of-property-conflict-v3.yml'))
-		.rejects.toThrow('Cannot merge properties')
+test('property conflict resolved', async() => {
+	const result = await createTestDocument('all-of-property-conflict-v3.yml')
+	const child = idx.get(result.models, 'Child')
+
+	expect(child).toBeDefined()
+	expect(child!.properties).toBeDefined()
+
+	const property = idx.get(child!.properties!, 'childName')
+	expect(property).toBeDefined()
+	expect(property!.propertyType).toEqual(CodegenPropertyType.NUMBER)
 })

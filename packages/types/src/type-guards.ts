5e3788f02840e8095c8b6ab6eef9af76d27ce4ac
Karl von Randow <karl@xk72.com>
2020-12-06 21:34:08 +1300

    types: add type guard functions for CodegenSchema

---
+++ packages/types/src/type-guards.ts
@@ -0,0 +1,35 @@
+import { CodegenArraySchema, CodegenBooleanSchema, CodegenEnumSchema, CodegenMapSchema, CodegenNumericSchema, CodegenObjectSchema, CodegenSchema, CodegenSchemaType, CodegenScope, CodegenStringSchema } from './types'
+
+export function isCodegenNumericSchema(schema: CodegenSchema): schema is CodegenNumericSchema {
+	return schema.schemaType === CodegenSchemaType.NUMBER || schema.schemaType == CodegenSchemaType.INTEGER
+}
+
+export function isCodegenBooleanSchema(schema: CodegenSchema): schema is CodegenBooleanSchema {
+	return schema.schemaType === CodegenSchemaType.BOOLEAN
+}
+
+export function isCodegenStringSchema(schema: CodegenSchema): schema is CodegenStringSchema {
+	return schema.schemaType === CodegenSchemaType.STRING || schema.schemaType == CodegenSchemaType.DATE ||
+		schema.schemaType === CodegenSchemaType.DATETIME || schema.schemaType === CodegenSchemaType.TIME
+}
+
+export function isCodegenArraySchema(schema: CodegenSchema): schema is CodegenArraySchema {
+	return schema.schemaType === CodegenSchemaType.ARRAY
+}
+
+export function isCodegenMapSchema(schema: CodegenSchema): schema is CodegenMapSchema {
+	return schema.schemaType === CodegenSchemaType.MAP
+}
+
+export function isCodegenEnumSchema(schema: CodegenSchema): schema is CodegenEnumSchema {
+	return schema.schemaType === CodegenSchemaType.ENUM
+}
+
+export function isCodegenObjectSchema(schema: CodegenSchema): schema is CodegenObjectSchema {
+	return schema.schemaType === CodegenSchemaType.OBJECT
+}
+
+export function isCodegenScope(schema: CodegenSchema): schema is CodegenSchema & CodegenScope {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	return (schema as any).scopedName
+}

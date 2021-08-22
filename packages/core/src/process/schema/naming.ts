karlvr/openapi-generator-plus

commit 86e1cff4890861b3a472849c6976550c299550d4

objects: convert property names to identifiers and ensure uniqueness
Introduces serializedName for CodegenProperty to represent the name as it should be serialized

packages/core/src/process/schema/naming.ts
@@ -1,9 +1,10 @@
-import { CodegenNamedSchema, CodegenSchema, CodegenSchemaNameOptions, CodegenScope } from '@openapi-generator-plus/types'
+import { CodegenNamedSchema, CodegenSchema, CodegenSchemaNameOptions, CodegenSchemaType, CodegenScope, IndexedCollectionType } from '@openapi-generator-plus/types'
 import { InternalCodegenState } from '../../types'
 import { OpenAPIX } from '../../types/patches'
 import { toCodegenVendorExtensions } from '../vendor-extensions'
 import { toCodegenSchemaTypeFromSchema } from './schema-type'
 import { nameFromRef } from '../utils'
+import * as idx from '@openapi-generator-plus/indexed-type'
 
 export interface ScopedModelInfo {
 	name: string
@@ -121,4 +122,24 @@ export function extractNaming(naming: ScopedModelInfo | null): Pick<CodegenSchem
 		scopedName: naming.scopedName,
 		serializedName: naming.serializedName,
 	}
+}
+
+type TestUniqueNameFunc = (name: string, parentNames: string[] | undefined) => boolean
+
+export function toUniqueName(suggestedName: string, parentNames: string[] | undefined, existingNames: IndexedCollectionType<unknown> | null, state: InternalCodegenState): string
+export function toUniqueName(suggestedName: string, parentNames: string[] | undefined, testUniqueName: TestUniqueNameFunc, state: InternalCodegenState): string 
+export function toUniqueName(suggestedName: string, parentNames: string[] | undefined, testOrData: IndexedCollectionType<unknown> | TestUniqueNameFunc | null, state: InternalCodegenState): string {
+	if (!testOrData) {
+		return suggestedName
+	}
+
+	const testUniqueName = typeof testOrData === 'function' ? testOrData : (possibleName: string) => !idx.has(testOrData, possibleName)
+
+	let name = suggestedName
+	let iteration = 0
+	while (!testUniqueName(name, parentNames)) {
+		iteration += 1
+		name = state.generator.toIteratedSchemaName(suggestedName, parentNames, iteration)
+	}
+	return name
 }

packages/core/src/process/schema/object.ts
@@ -4,7 +4,7 @@ import { InternalCodegenState } from '../../types'
 import { OpenAPIX } from '../../types/patches'
 import { extractCodegenTypeInfo } from '../utils'
 import { toCodegenVendorExtensions } from '../vendor-extensions'
-import { extractNaming, fullyQualifiedName, ScopedModelInfo, toUniqueScopedName } from './naming'
+import { extractNaming, fullyQualifiedName, ScopedModelInfo, toUniqueName, toUniqueScopedName } from './naming'
 import { addToKnownSchemas, extractCodegenSchemaCommon } from './utils'
 import * as idx from '@openapi-generator-plus/indexed-type'
 import { toCodegenSchemaUsage } from './index'
@@ -443,7 +443,7 @@ function toCodegenProperties(schema: OpenAPIX.SchemaObject, scope: CodegenScope,
 
 		const propertySchema = schema.properties[propertyName]
 		const property = toCodegenProperty(propertyName, propertySchema, required, scope, state)
-		idx.set(properties, property.name, property)
+		addCodegenProperty(properties, property, state)
 
 		if (required) {
 			requiredPropertyNames.splice(requiredIndex, 1)
@@ -455,6 +455,21 @@ function toCodegenProperties(schema: OpenAPIX.SchemaObject, scope: CodegenScope,
 	}
 
 	return idx.undefinedIfEmpty(properties)
+}
+
+/**
+ * Add the given property to the given set of object properties. Ensures that the property name is unique within the set of properties.
+ * Note that property names are unique in the spec, but may not be when converted to identifiers for the current generator.
+ * @param properties the object properties
+ * @param property the property to add
+ * @param state 
+ * @returns 
+ */
+export function addCodegenProperty(properties: CodegenProperties, property: CodegenProperty, state: InternalCodegenState): CodegenProperty {
+	const uniquePropertyName = toUniqueName(property.name, undefined, properties, state)
+	property.name = uniquePropertyName
+	idx.set(properties, property.name, property)
+	return property
 }
 
 function toCodegenProperty(name: string, schema: OpenAPIX.SchemaObject, required: boolean, scope: CodegenScope | null, state: InternalCodegenState): CodegenProperty {
@@ -469,7 +484,8 @@ function toCodegenProperty(name: string, schema: OpenAPIX.SchemaObject, required
 	})
 	return {
 		...schemaUsage,
-		name,
+		name: state.generator.toIdentifier(name),
+		serializedName: name,
 		description: description || schemaUsage.schema.description || null,
 		initialValue: schemaUsage.defaultValue || state.generator.initialValue(schemaUsage) || null,
 	}

packages/types/src/types.ts
@@ -358,6 +358,8 @@ export interface CodegenSchemaUsage extends CodegenSchemaInfo {
 
 export interface CodegenProperty extends CodegenSchemaUsage {
 	name: string
+	/** The name of the property in the API spec as it should be used when serialized (e.g. in JSON) */
+	serializedName: string
 	description: string | null
 
 	/** The initial value that the property should have. This is either the defaultValue, if there is one, or a default default from the generator. */
@@ -510,6 +512,10 @@ export interface CodegenObjectSchema extends CodegenNamedSchema, CodegenScope {
 
 export type CodegenObjectSchemas = IndexedCollectionType<CodegenObjectSchema>
 
+/**
+ * The set of properties for an object. The keys are the property names converted
+ * to safe identifiers by the generator.
+ */
 export type CodegenProperties = IndexedCollectionType<CodegenProperty>
 
 export interface CodegenDiscriminatorReference {

packages/core/src/__tests__/odd-models.spec.ts
@@ -1,6 +1,6 @@
-import { createTestDocument } from './common'
+import { createTestDocument, createTestGenerator } from './common'
 import { idx } from '../'
-import { CodegenSchemaType } from '../../../types/dist'
+import { CodegenObjectSchema, CodegenObjectSchemas, CodegenSchemaType } from '../../../types/dist'
 
 test('array of strings without collection models', async() => {
 	const result = await createTestDocument('odd-models/array-of-strings-v2.yml')
@@ -34,4 +34,50 @@ test('missing schema', async() => {
 	expect(op.responses![200]).toBeDefined()
 	expect(op.responses![200].headers!['ResponseHeader']).toBeDefined()
 	expect(op.responses![200].headers!['ResponseHeader'].schemaType).toEqual(CodegenSchemaType.STRING)
+})
+
+test('property names not legal identifiers', async() => {
+	const result = await createTestDocument('odd-models/property-name-not-identifier-safe.yml')
+	const generator = createTestGenerator()
+
+	const op = result.groups[0].operations[0]
+	const schema = op.defaultResponse!.defaultContent!.schema as CodegenObjectSchema
+	expect(schema).toBeDefined()
+
+	const propertyName = generator.toIdentifier('a-hyphenated-property')
+	expect(propertyName).not.toEqual('a-hyphenated-property')
+	
+	const property = idx.get(schema.properties!, propertyName)
+	expect(property).toBeDefined()
+	expect(property!.name).toEqual(propertyName)
+	expect(property!.serializedName).toEqual('a-hyphenated-property')
+})
+
+test('property names not legal identifiers non-unique', async() => {
+	const result = await createTestDocument('odd-models/property-name-not-identifier-safe-non-unique.yml')
+	const generator = createTestGenerator()
+
+	const op = result.groups[0].operations[0]
+	const schema = op.defaultResponse!.defaultContent!.schema as CodegenObjectSchema
+	expect(schema).toBeDefined()
+
+	expect(idx.size(schema.properties!)).toBe(2)
+
+	const propertyName = generator.toIdentifier('a-hyphenated-property')
+	const propertyName2 = generator.toIdentifier('a-hyphenated-Property')
+	expect(propertyName).not.toEqual('a-hyphenated-property')
+	expect(propertyName).toEqual(propertyName2)
+	
+	console.log(schema.properties)
+
+	const property = idx.get(schema.properties!, propertyName)
+	expect(property).toBeDefined()
+	expect(property!.name).toEqual(propertyName)
+	expect(property!.serializedName).toEqual('a-hyphenated-property')
+
+	const actualPropertyName2 = generator.toIteratedSchemaName(propertyName2, undefined, 1)
+	const property2 = idx.get(schema.properties!, actualPropertyName2)
+	expect(property2).toBeDefined()
+	expect(property2!.name).toEqual(actualPropertyName2)
+	expect(property2!.serializedName).toEqual('a-hyphenated-Property')
 })

packages/core/src/__tests__/common.ts
@@ -3,6 +3,7 @@ import { addToGroupsByPath } from '../operation-grouping'
 import { constructGenerator, createCodegenState, createCodegenDocument, createCodegenInput } from '..'
 import path from 'path'
 import pluralize from 'pluralize'
+import { camelCase } from 'lodash'
 
 interface TestCodegenOptions {
 	config: TestCodegenConfig
@@ -20,7 +21,7 @@ const testGeneratorConstructor: CodegenGeneratorConstructor = (config, generator
 	return {
 		generatorType: () => CodegenGeneratorType.SERVER,
 		toClassName: (name) => `${name}_class`,
-		toIdentifier: (name) => `${name.replace(/[^a-zA-Z0-9_]/g, '_')}`,
+		toIdentifier: (name) => camelCase(`${name.replace(/[^a-zA-Z0-9_]/g, '_')}`),
 		toConstantName: (name) => `${name}_constant`,
 		toEnumMemberName: (name) => `${name.replace('-', '')}_enum_member`,
 		toOperationName: (path, method) => `${method} ${path} operation`,

packages/core/src/__tests__/odd-models/property-name-not-identifier-safe-non-unique.yml
@@ -1,0 +1,22 @@
+---
+# Not all object properties have names that are safe as identifiers
+openapi: 3.0.3
+info:
+  title: Example
+  version: '1.0.1'
+paths:
+  /test1:
+    get:
+      responses:
+        200:
+          description: OK
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  a-hyphenated-property:
+                    type: string
+                  a-hyphenated-Property:
+                    type: string
+                    

packages/core/src/__tests__/odd-models/property-name-not-identifier-safe.yml
@@ -1,0 +1,20 @@
+---
+# Not all object properties have names that are safe as identifiers
+openapi: 3.0.3
+info:
+  title: Example
+  version: '1.0.1'
+paths:
+  /test1:
+    get:
+      responses:
+        200:
+          description: OK
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  a-hyphenated-property:
+                    type: string
+                    

packages/indexed-type/src/maps.ts
@@ -64,6 +64,10 @@ export function get<K, V>(map: Map<K, V>, key: K): V | undefined {
 	return map.get(key)
 }
 
+export function has<K, V>(map: Map<K, V>, key: K): boolean {
+	return map.has(key)
+}
+
 export function allKeys<K, V>(map: Map<K, V>): K[] {
 	return [...map.keys()]
 }

commit 5eabafa50b146d57869f35dba05b948efcb19c69

objects: revert to using raw property name in properties collection
Regression from 86e1cff4890861b3a472849c6976550c299550d4. We need to use the original property name from the spec, as things refer to it (such as the upcoming encoding work), therefore we need to ensure uniqueness of the name _inside_ the property, knowing it might not be the same as the key the property is under.

packages/core/src/process/schema/naming.ts
@@ -123,15 +123,18 @@ export function extractNaming(naming: ScopedModelInfo | null): Pick<CodegenSchem
 }
 
 type TestUniqueNameFunc = (name: string, parentNames: string[] | undefined) => boolean
+interface WithName {
+	name: string
+}
 
-export function toUniqueName(suggestedName: string, parentNames: string[] | undefined, existingNames: IndexedCollectionType<unknown> | null, state: InternalCodegenState): string
+export function toUniqueName(suggestedName: string, parentNames: string[] | undefined, existingNames: IndexedCollectionType<WithName> | null, state: InternalCodegenState): string
 export function toUniqueName(suggestedName: string, parentNames: string[] | undefined, testUniqueName: TestUniqueNameFunc, state: InternalCodegenState): string 
-export function toUniqueName(suggestedName: string, parentNames: string[] | undefined, testOrData: IndexedCollectionType<unknown> | TestUniqueNameFunc | null, state: InternalCodegenState): string {
+export function toUniqueName(suggestedName: string, parentNames: string[] | undefined, testOrData: IndexedCollectionType<WithName> | TestUniqueNameFunc | null, state: InternalCodegenState): string {
 	if (!testOrData) {
 		return suggestedName
 	}
 
-	const testUniqueName = typeof testOrData === 'function' ? testOrData : (possibleName: string) => !idx.has(testOrData, possibleName)
+	const testUniqueName = typeof testOrData === 'function' ? testOrData : (possibleName: string) => uniqueNameInIndexedCollection(possibleName, testOrData)
 
 	let name = suggestedName
 	let iteration = 0
@@ -140,4 +143,19 @@ export function toUniqueName(suggestedName: string, parentNames: string[] | unde
 		name = state.generator.toIteratedSchemaName(suggestedName, parentNames, iteration)
 	}
 	return name
+}
+
+/**
+ * Returns `true` if the given name is unique in the collection of objects with names.
+ * @param name a possible name
+ * @param collection a collection of objects with names
+ * @returns 
+ */
+function uniqueNameInIndexedCollection(name: string, collection: IndexedCollectionType<WithName>): boolean {
+	for (const value of idx.allValues(collection)) {
+		if (value.name === name) {
+			return false
+		}
+	}
+	return true
 }

packages/core/src/process/schema/object.ts
@@ -469,7 +469,11 @@ function toCodegenProperties(schema: OpenAPIX.SchemaObject, scope: CodegenScope,
 export function addCodegenProperty(properties: CodegenProperties, property: CodegenProperty, state: InternalCodegenState): CodegenProperty {
 	const uniquePropertyName = toUniqueName(property.name, undefined, properties, state)
 	property.name = uniquePropertyName
-	idx.set(properties, property.name, property)
+
+	if (idx.has(properties, property.serializedName)) {
+		throw new Error(`properties already includes "${property.serializedName}" in ${properties}`)
+	}
+	idx.set(properties, property.serializedName, property)
 	return property
 }
 

packages/types/src/types.ts
@@ -513,8 +513,7 @@ export interface CodegenObjectSchema extends CodegenNamedSchema, CodegenScope {
 export type CodegenObjectSchemas = IndexedCollectionType<CodegenObjectSchema>
 
 /**
- * The set of properties for an object. The keys are the property names converted
- * to safe identifiers by the generator.
+ * The set of properties for an object. The keys are the property names from the spec.
  */
 export type CodegenProperties = IndexedCollectionType<CodegenProperty>
 

packages/core/src/__tests__/odd-models.spec.ts
@@ -1,6 +1,6 @@
 import { createTestDocument, createTestGenerator } from './common'
 import { idx } from '../'
-import { CodegenObjectSchema, CodegenObjectSchemas, CodegenSchemaType } from '../../../types/dist'
+import { CodegenObjectSchema, CodegenSchemaType } from '../../../types/dist'
 
 test('array of strings without collection models', async() => {
 	const result = await createTestDocument('odd-models/array-of-strings-v2.yml')
@@ -47,7 +47,7 @@ test('property names not legal identifiers', async() => {
 	const propertyName = generator.toIdentifier('a-hyphenated-property')
 	expect(propertyName).not.toEqual('a-hyphenated-property')
 	
-	const property = idx.get(schema.properties!, propertyName)
+	const property = idx.get(schema.properties!, 'a-hyphenated-property')
 	expect(property).toBeDefined()
 	expect(property!.name).toEqual(propertyName)
 	expect(property!.serializedName).toEqual('a-hyphenated-property')
@@ -68,13 +68,13 @@ test('property names not legal identifiers non-unique', async() => {
 	expect(propertyName).not.toEqual('a-hyphenated-property')
 	expect(propertyName).toEqual(propertyName2)
 	
-	const property = idx.get(schema.properties!, propertyName)
+	const property = idx.get(schema.properties!, 'a-hyphenated-property')
 	expect(property).toBeDefined()
 	expect(property!.name).toEqual(propertyName)
 	expect(property!.serializedName).toEqual('a-hyphenated-property')
 
 	const actualPropertyName2 = generator.toIteratedSchemaName(propertyName2, undefined, 1)
-	const property2 = idx.get(schema.properties!, actualPropertyName2)
+	const property2 = idx.get(schema.properties!, 'a-hyphenated-Property')
 	expect(property2).toBeDefined()
 	expect(property2!.name).toEqual(actualPropertyName2)
 	expect(property2!.serializedName).toEqual('a-hyphenated-Property')
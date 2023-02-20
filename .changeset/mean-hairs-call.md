6717fb3273994d79a0e6cf3db974f988bbcb4fe9
Karl von Randow <karl@cactuslab.com>
2023-02-21 12:15:10 +1300

    cli: Add missing --clean option to usage

---
+++ .changeset/mean-hairs-call.md
@@ -0,0 +1,5 @@
+---
+"openapi-generator-plus": patch
+---
+
+Add missing --clean option to usage
--- packages/cli/src/usage.ts
+++ packages/cli/src/usage.ts
@@ -1,3 +1,3 @@
 export function usage(): void {
-	console.log(`usage: ${process.argv[1]} [generate] [-c <config file>] [-o <output dir>] [-g <generator template or path>] [--watch] [<path or url to api spec>]`)
+	console.log(`usage: ${process.argv[1]} [generate] [-c <config file>] [-o <output dir>] [-g <generator template or path>] [--watch] [--clean] [<path or url to api spec>]`)
 }

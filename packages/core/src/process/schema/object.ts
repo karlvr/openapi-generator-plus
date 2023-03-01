5adec2c8927e1e67786f016fc4743c75bb0b46af
Karl von Randow <karl@cactuslab.com>
2023-03-01 14:06:24 +1300

    core: Nested map schema should be anonymous rather than assuming the name of the object
    There wasn’t any impact, as the object replaced it shortly afterwards, but we’re about to detect replacements like this as they should represent generator failures.

--- packages/core/src/process/schema/object.ts
+++ packages/core/src/process/schema/object.ts
@@ -165,7 +165,7 @@ function handleObjectCommon<T extends CodegenObjectSchema | CodegenInterfaceSche
 	if (apiSchema.additionalProperties) {
 		/* This schema also has additional properties */
 		try {
-			const mapSchema = toCodegenMapSchema(apiSchema, naming, 'value', schema, state)
+			const mapSchema = toCodegenMapSchema(apiSchema, null, 'value', schema, state)
 			schema.additionalProperties = mapSchema
 		} catch (error) {
 			state.log(CodegenLogLevel.WARN, `Failed to generate additional property schema for ${naming.name} ${schema.additionalProperties}: ${(error as Error).message}`)

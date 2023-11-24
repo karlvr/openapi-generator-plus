01bb97d1e8153c8282e7087a59ff44d1e99a8bdc
Karl von Randow <karl@cactuslab.com>
2023-11-25 07:34:22 +1300

    Fix response type compile problem with updated openapi-types

--- packages/core/src/types/patches.d.ts
+++ packages/core/src/types/patches.d.ts
@@ -1,8 +1,8 @@
-import { OpenAPIV2, OpenAPIV3 } from 'openapi-types'
+import { OpenAPIV2, OpenAPIV3, OpenAPIV3_1 } from 'openapi-types'
 
 export declare namespace OpenAPIX {
 	export type ResponsesObject = OpenAPIV2.ResponsesObject | OpenAPIV3.ResponsesObject
-	export type Response = OpenAPIV2.Response | OpenAPIV3.ResponseObject | OpenAPIV3.ReferenceObject
+	export type Response = OpenAPIV2.Response | OpenAPIV3.ResponseObject | OpenAPIV3_1.ResponseObject | OpenAPIV3.ReferenceObject | OpenAPIV3_1.ReferenceObject
 	export type SchemaObject = OpenAPIV2.SchemaObject | OpenAPIV3.SchemaObject | OpenAPIV2.GeneralParameterObject
 	export type ReferenceObject = OpenAPIV2.ReferenceObject | OpenAPIV3.ReferenceObject
 	export type Parameters = OpenAPIV2.Parameters | (OpenAPIV3.ReferenceObject | OpenAPIV3.ParameterObject)[]

5b5d74c8db6d4b354533e1154ef7c89c5c8f0245
Nathan Zhou <nathanz@cactuslab.com>
2024-07-01 15:48:16 +1200

    refactor: change null to undefined
    And improve some wording.

--- packages/utils/src/content.ts
+++ packages/utils/src/content.ts
@@ -1,17 +1,17 @@
 import { CodegenSchema, isCodegenObjectLikeSchema, isCodegenArraySchema } from '@openapi-generator-plus/types'
 
 /**
- * Return the "value" schema for a metadata schema that might be an object schema or array schema.
- * Otherwise return null if such a schema cannot be found.
+ * Return the "value" schema for a metadata schema that might be either single-valued or an array.
+ * Otherwise return undefined if such a schema cannot be found.
  * @param schema 
  * @returns 
  */
-export function valueSchemaForMetadataSchema(schema: CodegenSchema): CodegenSchema | null {
+export function valueSchemaForMetadataSchema(schema: CodegenSchema): CodegenSchema | undefined {
 	if (isCodegenObjectLikeSchema(schema) && schema.properties && schema.properties.value) {
 		return schema.properties.value.schema
 	} else if (isCodegenArraySchema(schema) && schema.component && isCodegenObjectLikeSchema(schema.component.schema) && schema.component.schema.properties && schema.component.schema.properties.value) {
 		return schema.component.schema.properties.value.schema
 	} else {
-		return null
+		return undefined
 	}
 }
